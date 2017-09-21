9ae64038ac6e385ea6d6e2f2d283c52c473fdaae
Joscha <joscha@virtual-artz.de>
2017-09-21 15:17:13 +0200

    working sightly ceaned up version for the Mega

--- sensor10/SDS011.cpp
+++ sensor10/SDS011.cpp
@@ -11,25 +11,25 @@
 #include "SDS011.h"
 
 static const byte SLEEPCMD[19] = {
-	0xAA,	// head
-	0xB4,	// command id
-	0x06,	// data byte 1
-	0x01,	// data byte 2 (set mode)
-	0x00,	// data byte 3 (sleep)
-	0x00,	// data byte 4
-	0x00,	// data byte 5
-	0x00,	// data byte 6
-	0x00,	// data byte 7
-	0x00,	// data byte 8
-	0x00,	// data byte 9
-	0x00,	// data byte 10
-	0x00,	// data byte 11
-	0x00,	// data byte 12
-	0x00,	// data byte 13
-	0xFF,	// data byte 14 (device id byte 1)
-	0xFF,	// data byte 15 (device id byte 2)
-	0x05,	// checksum
-	0xAB	// tail
+  0xAA,	// head
+  0xB4,	// command id
+  0x06,	// data byte 1
+  0x01,	// data byte 2 (set mode)
+  0x00,	// data byte 3 (sleep)
+  0x00,	// data byte 4
+  0x00,	// data byte 5
+  0x00,	// data byte 6
+  0x00,	// data byte 7
+  0x00,	// data byte 8
+  0x00,	// data byte 9
+  0x00,	// data byte 10
+  0x00,	// data byte 11
+  0x00,	// data byte 12
+  0x00,	// data byte 13
+  0xFF,	// data byte 14 (device id byte 1)
+  0xFF,	// data byte 15 (device id byte 2)
+  0x05,	// checksum
+  0xAB	// tail
 };
 
 SDS011::SDS011(void) {
@@ -40,71 +40,79 @@ SDS011::SDS011(void) {
 // SDS011:read
 // --------------------------------------------------------
 int SDS011::read(float *p25, float *p10) {
-	byte buffer;
-	int value;
-	int len = 0;
-	int pm10_serial = 0;
-	int pm25_serial = 0;
-	int checksum_is;
-	int checksum_ok = 0;
-	int error = 1;
-	while ((sds_data->available() > 0) && (sds_data->available() >= (10-len))) {
-		buffer = sds_data->read();
-		value = int(buffer);
-		switch (len) {
-			case (0): if (value != 170) { len = -1; }; break;
-			case (1): if (value != 192) { len = -1; }; break;
-			case (2): pm25_serial = value; checksum_is = value; break;
-			case (3): pm25_serial += (value << 8); checksum_is += value; break;
-			case (4): pm10_serial = value; checksum_is += value; break;
-			case (5): pm10_serial += (value << 8); checksum_is += value; break;
-			case (6): checksum_is += value; break;
-			case (7): checksum_is += value; break;
-			case (8): if (value == (checksum_is % 256)) { checksum_ok = 1; } else { len = -1; }; break;
-			case (9): if (value != 171) { len = -1; }; break;
-		}
-		len++;
-		if (len == 10 && checksum_ok == 1) {
-			*p10 = (float)pm10_serial/10.0;
-			*p25 = (float)pm25_serial/10.0;
-			len = 0; checksum_ok = 0; pm10_serial = 0.0; pm25_serial = 0.0; checksum_is = 0;
-			error = 0;
-		}
-		yield();
-	}
-	return error;
+  byte buffer;
+  int value;
+  int len = 0;
+  int pm10_serial = 0;
+  int pm25_serial = 0;
+  int checksum_is;
+  int checksum_ok = 0;
+  int error = 1;
+  while ((Serial2.available() > 0) && (Serial2.available() >= (10 - len))) {
+    buffer = Serial2.read();
+    value = int(buffer);
+    switch (len) {
+      case (0): if (value != 170) {
+          len = -1;
+        }; break;
+      case (1): if (value != 192) {
+          len = -1;
+        }; break;
+      case (2): pm25_serial = value; checksum_is = value; break;
+      case (3): pm25_serial += (value << 8); checksum_is += value; break;
+      case (4): pm10_serial = value; checksum_is += value; break;
+      case (5): pm10_serial += (value << 8); checksum_is += value; break;
+      case (6): checksum_is += value; break;
+      case (7): checksum_is += value; break;
+      case (8): if (value == (checksum_is % 256)) {
+          checksum_ok = 1;
+        } else {
+          len = -1;
+        }; break;
+      case (9): if (value != 171) {
+          len = -1;
+        }; break;
+    }
+    len++;
+    if (len == 10 && checksum_ok == 1) {
+      *p10 = (float)pm10_serial / 10.0;
+      *p25 = (float)pm25_serial / 10.0;
+      len = 0; checksum_ok = 0; pm10_serial = 0.0; pm25_serial = 0.0; checksum_is = 0;
+      error = 0;
+    }
+    yield();
+  }
+  return error;
 }
 
 // --------------------------------------------------------
 // SDS011:sleep
 // --------------------------------------------------------
 void SDS011::sleep() {
-	for (uint8_t i = 0; i < 19; i++) {
-		sds_data->write(SLEEPCMD[i]);
-	}
-	sds_data->flush();
-	while (sds_data->available() > 0) {
-		sds_data->read();
-	}
+  for (uint8_t i = 0; i < 19; i++) {
+    Serial2.write(SLEEPCMD[i]);
+  }
+  Serial2.flush();
+  while (Serial2.available() > 0) {
+    Serial2.read();
+  }
 }
 
 // --------------------------------------------------------
 // SDS011:wakeup
 // --------------------------------------------------------
 void SDS011::wakeup() {
-	sds_data->write(0x01);
-	sds_data->flush();
+  sds_data->flush();*/
+  Serial2.write(0x01);
+  Serial2.flush();
 }
 
 void SDS011::begin(uint8_t pin_rx, uint8_t pin_tx) {
-	_pin_rx = pin_rx;
-	_pin_tx = pin_tx;
+  _pin_rx = pin_rx;
+  _pin_tx = pin_tx;
 
-	SoftwareSerial *softSerial = new SoftwareSerial(_pin_rx, _pin_tx);
+  //Initialize the 'Wire' class for I2C-bus communication.
 
-	//Initialize the 'Wire' class for I2C-bus communication.
-	softSerial->begin(9600);
-
-	sds_data = softSerial;
+  Serial2.begin(9600);
 }
 
--- sensor10/sensor10.ino
+++ sensor10/sensor10.ino
@@ -1,33 +1,33 @@
 /*
- * sensor10 - test program
- * originated @ cyclehack berlin 2017 
- * 
- */
+   sensor10
+   originated @ cyclehack berlin 2017
 
-// include all libraries
-#include "SDS011.h"
+*/
+
+// libraries
+// TTN, lmic and hal are available via Library management tool of the Arduino IDE
 #include <TheThingsNetwork.h>
-#include <lmic.h>
-#include "ttnKeys.h"
-#include <hal/hal.h>
+#include "SDS011.h" // SDS011 Particulate Matter Sensor
+#include <lmic.h> // LoRaWAN
+#include <hal/hal.h> // Harware Abstraction Library
 #include <TinyGPS.h> // Download at http://arduiniana.org/libraries/tinygps/
-#include <SoftwareSerial.h>
 
-// define PINS
+// your The Things Network keys
+#include "ttnKeys.h"
 
-#define RX 0
-#define TX 1
+// PINS
+#define RX 51
+#define TX 50
 const lmic_pinmap lmic_pins = {
-.nss = 10,
-.rxtx = LMIC_UNUSED_PIN,
-.rst = 9,
-.dio = {2, 6, 7}, // Specify pin numbers for DIO0, 1, 2
-// connected to D2, D6, D7
+  .nss = 10,
+  .rxtx = LMIC_UNUSED_PIN,
+  .rst = 9,
+  .dio = {2, 6, 7}, // Specify pin numbers for DIO0, 1, 2
+  // connected to D2, D6, D7
 };
 
-// define values for sensor data
-
-float p10,p25;
+// values for sensor data
+float p10, p25;
 int sds_error, nr_sat;
 unsigned long lati, longi;
 static uint8_t message[12];
@@ -38,7 +38,6 @@ static osjob_t sendjob;
 // define sensors
 SDS011 sds;
 TinyGPS gps;
-SoftwareSerial ss(3, 4); // Arduino RX, TX
 
 // These callbacks are only used in over-the-air activation, so they are
 // left empty here (we cannot leave them out completely unless
@@ -50,31 +49,25 @@ void os_getDevKey (u1_t* buf) { }
 static void smartdelay(unsigned long ms)
 {
   unsigned long start = millis();
-  do 
-  {
-    while (ss.available())
-    {
+  do {
+    while (Serial1.available()) {
       //ss.print(Serial.read());
-      gps.encode(ss.read());
+      gps.encode(Serial1.read());
     }
   } while (millis() - start < ms);
 }
 
-static void print_float(float val, float invalid, int len, int prec)
-{
-  if (val == invalid)
-  {
+static void print_float(float val, float invalid, int len, int prec) {
+  if (val == invalid) {
     while (len-- > 1)
       Serial.print('*');
     Serial.print(' ');
-  }
-  else
-  {
+  } else {
     Serial.print(val, prec);
     int vi = abs((int)val);
     int flen = prec + (val < 0.0 ? 2 : 1); // . and -
     flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
+    for (int i = flen; i < len; ++i)
       Serial.print(' ');
   }
   smartdelay(0);
@@ -88,168 +81,131 @@ static void print_int(unsigned long val, unsigned long invalid, int len)
   else
     sprintf(sz, "%ld", val);
   sz[len] = 0;
-  for (int i=strlen(sz); i<len; ++i)
+  for (int i = strlen(sz); i < len; ++i)
     sz[i] = ' ';
-  if (len > 0) 
-    sz[len-1] = ' ';
+  if (len > 0)
+    sz[len - 1] = ' ';
   Serial.print(sz);
   smartdelay(0);
 }
 
-static void print_date(TinyGPS &gps)
-{
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  unsigned long age;
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  if (age == TinyGPS::GPS_INVALID_AGE)
-    Serial.print("********** ******** ");
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
-        month, day, year, hour, minute, second);
-    Serial.print(sz);
-  }
-  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-  smartdelay(0);
-}
-
-static void print_str(const char *str, int len)
-{
-  int slen = strlen(str);
-  for (int i=0; i<len; ++i)
-    Serial.print(i<slen ? str[i] : ' ');
-  smartdelay(0);
-}
-
 void onEvent (ev_t ev) {
-    Serial.print(os_getTime());
-    Serial.print(": ");
-    switch(ev) {
-        case EV_SCAN_TIMEOUT:
-            Serial.println(F("EV_SCAN_TIMEOUT"));
-            break;
-        case EV_BEACON_FOUND:
-            Serial.println(F("EV_BEACON_FOUND"));
-            break;
-        case EV_BEACON_MISSED:
-            Serial.println(F("EV_BEACON_MISSED"));
-            break;
-        case EV_BEACON_TRACKED:
-            Serial.println(F("EV_BEACON_TRACKED"));
-            break;
-        case EV_JOINING:
-            Serial.println(F("EV_JOINING"));
-            break;
-        case EV_JOINED:
-            Serial.println(F("EV_JOINED"));
-            break;
-        case EV_RFU1:
-            Serial.println(F("EV_RFU1"));
-            break;
-        case EV_JOIN_FAILED:
-            Serial.println(F("EV_JOIN_FAILED"));
-            break;
-        case EV_REJOIN_FAILED:
-            Serial.println(F("EV_REJOIN_FAILED"));
-            break;
-            break;
-        case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-            if(LMIC.dataLen) {
-                // data received in rx slot after tx
-                Serial.print(F("Data Received: "));
-                Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
-                Serial.println();
-            }
-            // Schedule next transmission
-            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
-            break;
-        case EV_LOST_TSYNC:
-            Serial.println(F("EV_LOST_TSYNC"));
-            break;
-        case EV_RESET:
-            Serial.println(F("EV_RESET"));
-            break;
-        case EV_RXCOMPLETE:
-            // data received in ping slot
-            Serial.println(F("EV_RXCOMPLETE"));
-            break;
-        case EV_LINK_DEAD:
-            Serial.println(F("EV_LINK_DEAD"));
-            break;
-        case EV_LINK_ALIVE:
-            Serial.println(F("EV_LINK_ALIVE"));
-            break;
-         default:
-            Serial.println(F("Unknown event"));
-            break;
-    }
+  Serial.print(os_getTime());
+  Serial.print(": ");
+  switch (ev) {
+    case EV_SCAN_TIMEOUT:
+      Serial.println(F("EV_SCAN_TIMEOUT"));
+      break;
+    case EV_BEACON_FOUND:
+      Serial.println(F("EV_BEACON_FOUND"));
+      break;
+    case EV_BEACON_MISSED:
+      Serial.println(F("EV_BEACON_MISSED"));
+      break;
+    case EV_BEACON_TRACKED:
+      Serial.println(F("EV_BEACON_TRACKED"));
+      break;
+    case EV_JOINING:
+      Serial.println(F("EV_JOINING"));
+      break;
+    case EV_JOINED:
+      Serial.println(F("EV_JOINED"));
+      break;
+    case EV_RFU1:
+      Serial.println(F("EV_RFU1"));
+      break;
+    case EV_JOIN_FAILED:
+      Serial.println(F("EV_JOIN_FAILED"));
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println(F("EV_REJOIN_FAILED"));
+      break;
+      break;
+    case EV_TXCOMPLETE:
+      Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+      if (LMIC.dataLen) {
+        // data received in rx slot after tx
+        Serial.print(F("Data Received: "));
+        Serial.write(LMIC.frame + LMIC.dataBeg, LMIC.dataLen);
+        Serial.println();
+      }
+      // Schedule next transmission
+      os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
+      break;
+    case EV_LOST_TSYNC:
+      Serial.println(F("EV_LOST_TSYNC"));
+      break;
+    case EV_RESET:
+      Serial.println(F("EV_RESET"));
+      break;
+    case EV_RXCOMPLETE:
+      // data received in ping slot
+      Serial.println(F("EV_RXCOMPLETE"));
+      break;
+    case EV_LINK_DEAD:
+      Serial.println(F("EV_LINK_DEAD"));
+      break;
+    case EV_LINK_ALIVE:
+      Serial.println(F("EV_LINK_ALIVE"));
+      break;
+    default:
+      Serial.println(F("Unknown event"));
+      break;
+  }
 }
 
-void do_send(osjob_t* j){
-    // Check if there is not a current TX/RX job running
-    if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
-    } else {
-        // Prepare upstream data transmission at the next possible time.
-        LMIC_setTxData2(1, message, sizeof(message), 0);
-        Serial.println(F("Packet queued"));
-    }
-    // Next TX is scheduled after TX_COMPLETE event.
+void do_send(osjob_t* j) {
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  } else {
+    // Prepare upstream data transmission at the next possible time.
+    LMIC_setTxData2(1, message, sizeof(message), 0);
+    Serial.println(F("Packet queued"));
+  }
+  // Next TX is scheduled after TX_COMPLETE event.
 }
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println(F("Starting"));
-    ss.begin(9600); // SoftSerial port to get GPS data. 
-    while (!Serial) {
-     ;
-  };
-  Serial.println("Monitor Dragino LoRa GPS Shield Status");
-  Serial.print("Testing TinyGPS library v. "); Serial.println(TinyGPS::library_version());
-  Serial.println();
-  Serial.println("Sats HDOP Latitude  Longitude  Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum");
-  Serial.println("          (deg)     (deg)      Age                      Age  (m)    --- from GPS ----  ---- to London  ----  RX    RX        Fail");
-  //Serial.println("-------------------------------------------------------------------------------------------------------------------------------------");
+  Serial.begin(9600);
+  Serial.println(F("Starting"));
+  Serial1.begin(9600);
+/*  while (!Serial) {
+    ;
+  };*/
+  Serial.println("TinyGPS library v. "); Serial.println(TinyGPS::library_version());
   // init SDS
   sds.begin(RX, TX);
 
   Serial.println("SDS initialized");
   // LMIC init
-    os_init();
-    // Reset the MAC state. Session and pending data transfers will be discarded.
-    LMIC_reset();
-    
-    // Set static session parameters. Instead of dynamically establishing a session
-    // by joining the network, precomputed session parameters are be provided.
-    #ifdef PROGMEM
-    // On AVR, these values are stored in flash and only copied to RAM
-    // once. Copy them to a temporary buffer here, LMIC_setSession will
-    // copy them into a buffer of its own again.
-    uint8_t appskey[sizeof(APPSKEY)];
-    uint8_t nwkskey[sizeof(NWKSKEY)];
-    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
-    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
-    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
-    #else
-    // If not running an AVR with PROGMEM, just use the arrays directly
-    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
-    #endif
+  os_init();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
 
-    // Disable link check validation
-    LMIC_setLinkCheckMode(0);
+  // Set static session parameters. Instead of dynamically establishing a session
+  // by joining the network, precomputed session parameters are be provided.
+#ifdef PROGMEM
+  // On AVR, these values are stored in flash and only copied to RAM
+  // once. Copy them to a temporary buffer here, LMIC_setSession will
+  // copy them into a buffer of its own again.
+  uint8_t appskey[sizeof(APPSKEY)];
+  uint8_t nwkskey[sizeof(NWKSKEY)];
+  memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+  memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+  LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+#else
+  // If not running an AVR with PROGMEM, just use the arrays directly
+  LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+#endif
 
-    // Set data rate and transmit power (note: txpow seems to be ignored by the library)
-    LMIC_setDrTxpow(DR_SF7,14);
-    
-    Serial.println("LoRa setup done");
-    
-  // init GPS
+  // Disable link check validation
+  LMIC_setLinkCheckMode(0);
 
-  // init LoRa
+  // Set data rate and transmit power (note: txpow seems to be ignored by the library)
+  LMIC_setDrTxpow(DR_SF7, 14);
 
+  Serial.println("LoRa setup done");
 }
 
 void read_gps_data() {
@@ -261,40 +217,25 @@ void read_gps_data() {
   nr_sat = gps.satellites();
   print_int(gps.hdop(), TinyGPS::GPS_INVALID_HDOP, 5);
   gps.f_get_position(&flat, &flon, &age);
-  
+
   lati = flat * 1000000;
   longi = flon * 1000000;
-  Serial.println(flat);
   print_float(flat, TinyGPS::GPS_INVALID_F_ANGLE, 10, 6);
   print_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 11, 6);
-  /*print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-  print_date(gps);
-  print_float(gps.f_altitude(), TinyGPS::GPS_INVALID_F_ALTITUDE, 7, 2);
-  print_float(gps.f_course(), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
-  print_float(gps.f_speed_kmph(), TinyGPS::GPS_INVALID_F_SPEED, 6, 2);
-  print_str(gps.f_course() == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(gps.f_course()), 6);
-  print_int(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0xFFFFFFFF : (unsigned long)TinyGPS::distance_between(flat, flon, LONDON_LAT, LONDON_LON) / 1000, 0xFFFFFFFF, 9);
-  print_float(flat == TinyGPS::GPS_INVALID_F_ANGLE ? TinyGPS::GPS_INVALID_F_ANGLE : TinyGPS::course_to(flat, flon, LONDON_LAT, LONDON_LON), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
-  print_str(flat == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(TinyGPS::course_to(flat, flon, LONDON_LAT, LONDON_LON)), 6);
-*/
+
   gps.stats(&chars, &sentences, &failed);
-  print_int(chars, 0xFFFFFFFF, 6);
-  print_int(sentences, 0xFFFFFFFF, 10);
-  print_int(failed, 0xFFFFFFFF, 9);
-  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Serial.println("Reading GPS data");
   read_gps_data();
-  if(nr_sat > 3)
-  {
-    //Serial.println("Got GPS Fix; reading sds");
-    sds_error = sds.read(&p25,&p10);
-    if(!sds_error)
-    {
-      //Serial.println("sds successfull");
+  sds_error = sds.read(&p25, &p10);
+  Serial.print("pm10: ");
+  Serial.print(p10);
+  Serial.print(", pm2,5: ");
+  Serial.println(p25);
+
+  if (nr_sat >= 3 && lati != 1000000000) {
+    if (!sds_error) {
       message[0] = (lati & 0xFF000000) >> 24;
       message[1] = (lati & 0x00FF0000) >> 16;
       message[2] = (lati & 0x0000FF00) >> 8;
@@ -309,32 +250,13 @@ void loop() {
       int p25_copy = (int) p25;
       message[10] = (p25_copy & 0xFF00) >> 8;
       message[11] = (p25_copy & 0x00FF);
-      Serial.println(lati);
-      Serial.println(longi);
-      //Serial.println(p10_copy);
-      //Serial.println(p25_copy);
-      /*Serial.println(message[0]);
-      Serial.println(message[1]);
-      Serial.println(message[2]);
-      Serial.println(message[3]);
-      Serial.println(message[4]);
-      Serial.println(message[5]);
-      Serial.println(message[6]);
-      Serial.println(message[7]);
-      Serial.println(message[8]);
-      Serial.println(message[9]);
-      Serial.println(message[10]);
-      Serial.println(message[11]);*/
+
       do_send(&sendjob);
-    }
-    else
-    {
+    } else {
       Serial.println("error reading SDS");
     }
-  }
-  else
-  {
-    //Serial.println("GPS accuracy too low!");
+  } else {
+    Serial.println("GPS accuracy too low!");
   }
   os_runloop_once(); // LMIC needs this
   smartdelay(1000);
